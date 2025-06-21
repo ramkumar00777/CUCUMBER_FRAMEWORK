4a99c0bf90dc0195a485fc1042aec9207a1214f5
RAM KUMAR N <95617244+ramkumar00777@users.noreply.github.com>
2025-06-22 00:47:34 +0530

    Update TestRunner.java

--- src/test/java/com/qascript/TestRunner.java
+++ src/test/java/com/qascript/TestRunner.java
@@ -1,25 +1,18 @@
 package com.qascript;
 
-
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        features = {"src/test/resources/feature/ShoppingCart.feature"},// Path to .feature file
-        glue = {"com/qascript/StepDefs"},
-        plugin =
-                {
-                        "pretty",
-                        "html:target/cucumber-reports.html" ,
-                        "json:target/cucumber-reports.json"
-                },
-
+        features = "classpath:feature/ShoppingCart.feature", // ✅ works in Jenkins
+        glue = "com.qascript.StepDefs",                      // ✅ correct package notation
+        plugin = {
+                "pretty",
+                "html:target/cucumber-reports.html",
+                "json:target/cucumber-reports.json"
+        },
         publish = true
-
 )
-public class TestRunner
-{
-
-}
+public class TestRunner { }
