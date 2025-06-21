5d64029392a11f10996652fcb775f32d53fc9bb8
ra-mkum-ar <rk8526360@gmail.com>
2025-06-21 23:50:23 +0530

    Made changes in TestRunner.java

--- src/test/java/com/qascript/TestRunner.java
+++ src/test/java/com/qascript/TestRunner.java
@@ -8,10 +8,10 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = {"src/test/resources/feature/ShoppingCart.feature"},// Path to .feature file
+        glue = {"com/qascript/StepDefs"},
         plugin =
                 {
                         "pretty",
-                        
                         "html:target/cucumber-reports.html" ,
                         "json:target/cucumber-reports.json"
                 },
